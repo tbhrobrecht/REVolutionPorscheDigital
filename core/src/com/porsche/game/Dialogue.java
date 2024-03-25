d650adc8c8fc9a5995f5b0b5e250636e55763397
timorobrecht <timo.robrecht@tum.de>
2024-03-25 17:41:31 +0100

    Created a new class that facilitates creating dialogue between characters

---
+++ core/src/com/porsche/game/Dialogue.java
@@ -0,0 +1,49 @@
+package com.porsche.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class Dialogue {
+    private ShapeRenderer shapeRenderer;
+    private float filterTransparency;
+    private float textBubbleTransparency;
+
+
+    public Dialogue(float filterTransparency, float textBubbleTransparency) {
+        this.shapeRenderer = new ShapeRenderer();
+        this.filterTransparency = filterTransparency;
+        this.textBubbleTransparency = textBubbleTransparency;
+    }
+
+    /**
+     * Method sprays a transparent layer of white over the entire screen
+     */
+    public void renderBackgroundFilter() {
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(new Color(1, 1, 1, filterTransparency));
+        shapeRenderer.rect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        shapeRenderer.end();
+
+        Gdx.gl.glDisable(GL20.GL_BLEND);
+    }
+
+    /**
+     * Method creates the white text box, upon which the text gets printed on
+     */
+    public void renderTextBubble() {
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(new Color(1, 1, 1, textBubbleTransparency));
+        shapeRenderer.rect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight() * 0.5f);
+        shapeRenderer.end();
+
+        Gdx.gl.glDisable(GL20.GL_BLEND);
+    }
+}
